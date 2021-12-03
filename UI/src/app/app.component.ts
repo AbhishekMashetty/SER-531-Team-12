9a249eeb1d7d24a95597bc2a7148e8e783c54796
vsingh57 <vsingh57@asu.edu>
2021-12-03 00:07:44 -0700

    WIP

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -20,15 +20,35 @@ export class AppComponent {
 
 
   search() {
+    let outMap = {}
+    let inMap = {}
     this.data.sparql("SELECT * WHERE{ ?s ?p ?o . }").subscribe(res => {
       console.log(res);
+
       this.outputText = ''
       res['results']['bindings'].forEach((triple: any) => {
-        if (triple.o.value === 'http://www.w3.org/2002/07/owl#NamedIndividual') {
-          this.outputText += triple.s.value.split('#')[1] + '\n'
+        if (inMap[triple.o.value]) {
+          inMap[triple.o.value] += 1
+        } else {
+          inMap[triple.o.value] = 1
         }
+        if (outMap[triple.s.value]) {
+          outMap[triple.s.value] += 1
+        } else {
+          outMap[triple.s.value] = 1
+        }
+        // if (triple.o.value === 'http://www.w3.org/2002/07/owl#NamedIndividual') {
+        //   this.outputText += triple.s.value.split('#')[1] + '\n'
+        // }
+
+      })
+      console.log(inMap)
+      console.log(outMap)
+      Object.keys(outMap).forEach(x => {
+        this.outputText += x + ' ' + outMap[x].toString() + '\n'
       })
     })
+
 
   }
 
